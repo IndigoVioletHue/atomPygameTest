8d43e6f86f88693948d4c0c7f25cdb8d3fe2e34b
IndigoVioletHue <aaronbowring5@gmail.com>
2020-10-10 15:11:05 +0100

    Fixed land scrolling, kinda but moreso (Collision borders now line up in both instances.)

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ while running: #main gameloop. Kinda stolen?
     for i in range(len(staticObjects)):
         staticObjects[i].tick()
         if player.rect.x >= 1080 and player.kupx == False: #if the player x is a certain number, move the land.
-            staticObjects[i].setX(5)
-            staticObjects[i].KDPX()
-        elif player.rect.x <= 200 and player.kupx == False: #if the x value is smaller than 200 and the key ISNT up.
             staticObjects[i].setX(-5)
             staticObjects[i].KDPX()
-    pygame.display.update()
+        elif player.rect.x <= 100 and player.kupx == False: #if the x value is smaller than 200 and the key ISNT up.
+            staticObjects[i].setX(5)
+            staticObjects[i].KDPX()
+    pygame.display.update()#fancy lil uhhh lil uhhhh display update for ya
