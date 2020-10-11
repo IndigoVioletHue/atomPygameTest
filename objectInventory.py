7de1033177732ced0ca2a3db31fca6f603d61572
IndigoVioletHue <aaronbowring5@gmail.com>
2020-10-11 16:25:17 +0100

    Added (unused) objectInventory file for testing the concept

---
+++ objectInventory.py
@@ -0,0 +1,49 @@
+import createObject
+
+def createAllMoveable():
+    player = createObject.newMoveable() #player object creation
+    player.setParams(75, 50, 615, 450)
+    player.setColor(BLACK)
+    player.drawChar(screen)
+
+def createAllStatic():
+    land = createObject.newStatic() #land object in the middle, might ditch this idea.
+    staticObjects.append(land)
+    land.setParams(120, 1280, 0, 650)
+    land.setColor((124,124,124))
+    land.isGround()
+
+    land2 = createObject.newStatic() #right land
+    staticObjects.append(land2)
+    land2.setParams(120, 1280, 1280, 650)
+    land2.setColor((200,124,200))
+    land2.isGround()
+
+    leftLand = createObject.newStatic() #left land
+    staticObjects.append(leftLand)
+    leftLand.setParams(120, 1280, -1280, 650)
+    leftLand.setColor((124,200,124))
+    leftLand.isGround()
+
+    platform = createObject.newStatic() #platform
+    staticObjects.append(platform)
+    platform.setParams(50, 300, 50, 480)
+    platform.setColor((124,124,124))
+
+    platform2 = createObject.newStatic() #another platform2
+    staticObjects.append(platform2)
+    platform2.setParams(50, 300, 250, 280)
+    platform2.setColor((124,124,124))
+
+    platform3 = createObject.newStatic() #another platform2
+    staticObjects.append(platform3)
+    platform3.setParams(50, 300, 450, 80)
+    platform3.setColor((124,124,124))
+
+
+    land.drawChar(screen) #drawing all of the land characters, as originally it was done every gametick,
+    land2.drawChar(screen)#before the newStatic class had a tick function
+    leftLand.drawChar(screen)
+    platform.drawChar(screen)
+    platform2.drawChar(screen)
+    platform3.drawChar(screen)
